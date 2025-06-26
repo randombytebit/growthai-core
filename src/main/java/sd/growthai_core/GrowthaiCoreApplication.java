d81fa6fae2ba956ef2f5934402b8aa9c7418089d
Leo <cs.lwong4@gmail.com>
2025-06-26 12:05:54 +0800

    base commit

---
+++ src/main/java/sd/growthai_core/GrowthaiCoreApplication.java
@@ -0,0 +1,13 @@
+package sd.growthai_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowthaiCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowthaiCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sd/growthai_core/GrowthaiCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package sd.growthai_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowthaiCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
